6e878c49b1834811f358028a9148a4e86a4cda6a
Hramirez64 <hmramirez@uninorte.edu.co>
2022-09-27 22:17:34 -0400

    Con base de datos

---
+++ enviaremail.py
@@ -0,0 +1,16 @@
+from email.message import EmailMessage
+import smtplib
+def enviar_email(email_destino,codigo):
+    remitente = "hmramirez@uninorte.edu.co"
+    destinatario = email_destino
+    mensaje = "Ingrese el Siguiente Codigo: "+codigo+" Para Activar Su Cuenta"
+    email = EmailMessage()
+    email["From"] = remitente
+    email["To"] = destinatario
+    email["Subject"] = "Codigo de Activacion"
+    email.set_content(mensaje)
+    smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+    smtp.starttls()
+    smtp.login(remitente, "Mabel7932")
+    smtp.sendmail(remitente, destinatario, email.as_string())
+    smtp.quit()
